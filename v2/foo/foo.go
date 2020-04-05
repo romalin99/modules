62898111b9c571ef26da0bb48c0d1fb830fbf69c
romalin99 <romalin99@gmail.com>
2020-04-05 21:22:42 +0800

    v2  test

---
+++ v2/foo/foo.go
@@ -0,0 +1,7 @@
+package foo
+
+import "fmt"
+
+func Foo2() {
+	fmt.Println("Foo2 of module: github.com/romalin99/modules v2.0.0")
+}
---
+++ v2/go.mod
@@ -0,0 +1,3 @@
+module github.com/romalin99/modules/v2
+
+go 1.14
