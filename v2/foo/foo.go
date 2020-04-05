e4c58c4f84d0989c91642408e4474426e55e5c09
romalin99 <romalin99@gmail.com>
2020-04-05 21:26:04 +0800

    test v22

--- v2/foo/foo.go
+++ v2/foo/foo.go
@@ -3,5 +3,5 @@ package foo
 import "fmt"
 
 func Foo2() {
-	fmt.Println("Foo2 of module: github.com/romalin99/modules v2.0.0")
+	fmt.Println("Foo2 of module:孔维岗  github.com/romalin99/modules v2.0.0")
 }
